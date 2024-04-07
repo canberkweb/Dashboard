131fd8056d2453e5820fa9ba0c72087492df8787
canberkweb <cnbrk0643@gmail.com>
2024-04-07 18:26:28 +0200

    Fix CustomersPage Syntax Error

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,13 +1,4 @@
 export default function Page() {
     return <p>Dashboard/Customers Page
-
-POSTGRES_URL="postgres://default:ytLn62bTPgSR@ep-blue-mountain-a4w7a4ng-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
-POSTGRES_PRISMA_URL="postgres://default:ytLn62bTPgSR@ep-blue-mountain-a4w7a4ng-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15"
-POSTGRES_URL_NO_SSL="postgres://default:ytLn62bTPgSR@ep-blue-mountain-a4w7a4ng-pooler.us-east-1.aws.neon.tech:5432/verceldb"
-POSTGRES_URL_NON_POOLING="postgres://default:ytLn62bTPgSR@ep-blue-mountain-a4w7a4ng.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
-POSTGRES_USER="default"
-POSTGRES_HOST="ep-blue-mountain-a4w7a4ng-pooler.us-east-1.aws.neon.tech"
-POSTGRES_PASSWORD="ytLn62bTPgSR"
-POSTGRES_DATABASE="verceldb"
     </p>;
   }
